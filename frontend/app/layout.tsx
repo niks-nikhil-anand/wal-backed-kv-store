2e0ec5bc2ab13bb955a95b45644b045f112f2f77
niks-nikhil-anand <niks.anand.developer@gmail.com>
2026-01-10 23:53:53 +0530

    feat: Update layout and styles for improved UI structure and dark mode support

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +30,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-950`}
       >
-        {children}
+        <div className="relative flex min-h-[calc(100vh-6rem)] flex-col">
+          <Navbar />
+          <div className="flex-1">{children}</div>
+          <Footer />
+        </div>
       </body>
     </html>
   );
