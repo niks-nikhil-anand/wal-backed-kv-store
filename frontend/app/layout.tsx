c08d58a47bf546bf6c7f272be7b967aada2389fe
niks-nikhil-anand <niks.anand.developer@gmail.com>
2026-01-11 07:34:09 +0530

    feat: Integrate Sonner for toast notifications in layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
+import { Toaster } from 'sonner';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
           <div className="flex-1">{children}</div>
           <Footer />
         </div>
+        <Toaster position="top-right" />
       </body>
     </html>
   );
